6dc9e50c9bba0fae4abb817084339a6b2f856834
Rafael <player08901@gmail.com>
2025-02-12 18:47:47 -0300

    uwu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
         <h1>Chegou a hora de ficar Saudavel</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, inventore quidem. Ipsam numquam ipsum temporibus dolor quasi architecto odit hic iusto sunt, eveniet suscipit nemo commodi eligendi nulla nam obcaecati.</p>
         {/* se não ta com login aparece o botão do cadastro */}
+        wuw
       </section>
     </main>
   );
